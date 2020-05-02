659e0661767aeb7dd37b61b9326ba4f618973743
Darshan Singh <darshan.singh.tech@gmail.com>
2020-05-02 11:38:22 -0400

    It just works
    I copied and pasted a lot f this back and forth from a web based Python IDE so I could quickly run code. Piss off.
    
    Everything works fine now.

--- Drink.py
+++ Drink.py
@@ -1,15 +1,19 @@
 import time
 duration = float(input("How many minutes till you take a drink? "))
+# This converts duration from seconds to minutes
+
+if duration < 1:
+    print("Reminding you in", duration, "minute")
+
+elif duration > 1:
+    print("Reminding you in", duration, "minute")
+
+else:
+    print("Reminding you in", duration, "minutes")
 
 duration = duration * 60
-# This converts duration from seconds to minutes
-print_duration = duration / 60
-
-if print_duration == 1:
-    print("Reminding you in", print_duration, "minute")
-# if print_duration <= 
-#     print("Reminding you in", print_duration, "minutes")
 
 time.sleep(duration)
+
 print("Drink something")
 # Try and run this in the terminal via a command
