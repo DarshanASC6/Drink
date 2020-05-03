316e6af02751cca532a97586f95a7455b1c159c0
Darshan Singh <darshan.singh.tech@gmail.com>
2020-05-03 08:49:51 -0400

    Done but Git Bash doesn't want to work
    Everything is done.
    
    Code is annotated. The function runs forever, which is the idea. Yet for some reason, it won't run from a terminal command in Git Bash.

--- Drink.py
+++ Drink.py
@@ -1,15 +1,20 @@
 import time
 
 duration = float(input("How many minutes till you take a drink? "))
+# Asks the user how frequently they want to be reminded to take a drink
 
 if duration < 1:
     print("Reminding you in", duration, "minutes")
+# Basic grammer stuff because I'm picky
 
 elif duration > 1:
     print("Reminding you in", duration, "minutes")
+# Basic grammer stuff because I'm picky
 
 else:
     print("Reminding you in", duration, "minute")
+# Basic grammer stuff because I'm picky
+
 
 duration = duration * 60
 # This converts duration from seconds to minutes
@@ -17,4 +22,3 @@ duration = duration * 60
 while 1 == 1:
     time.sleep(duration)
     print("Drink something")
-# Find a way to stop this after a certain point
